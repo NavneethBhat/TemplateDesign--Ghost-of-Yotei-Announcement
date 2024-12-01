e11e937138771fcbce419d4d3cc52c0d2d0c0713
Navaneetha <navnithbhat007@gmail.com>
2024-12-01 18:33:59 +0530

    06:33 Pm

--- reactiveness.js
+++ reactiveness.js
@@ -5,12 +5,12 @@ const cards = document.querySelectorAll('.card');
 cards.forEach(card => {
     card.addEventListener('mouseover', () => {
         card.style.transform = 'scale(1.1)';
-        card.style.opacity = '0.8';
+       
     });
 
     card.addEventListener('mouseout', () => {
         card.style.transform = 'scale(1)';
-        card.style.opacity = '1';
+        
     });
 });
 
